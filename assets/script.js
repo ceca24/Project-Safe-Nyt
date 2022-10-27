f055acf1f39c65708cd4b72772d9fdcccf658427
Carter Matschek <carter.matschek@gmail.com>
2022-10-27 10:10:07 -0500

    updated js with api key

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // Use this key in your application by passing it with the key=API_KEY parameter.
 var APIkey = "AIzaSyCQ2C-bnOvVf1LWKDlWlQy0DSckI8OR-XI"
-// var searchEL =
+// var searchEL = document.getElementbyID("")
 var queryURL = `https://www.google.com/maps/embed/v1/place?key=AIzaSyCQ2C-bnOvVf1LWKDlWlQy0DSckI8OR-XI
 &q=${SearchEl}`
 
@@ -10,5 +10,4 @@ fetch(queryURL)
 
 
 
-// ID needs in html
-    // var cityEL = 'enter-city' ID in the search input
+
