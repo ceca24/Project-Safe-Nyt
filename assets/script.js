4507c0583271c1f68dcee12b766dc440df2a1740
Carter Matschek <carter.matschek@gmail.com>
2022-11-01 11:43:14 -0500

    map resize and commented out media queries

--- assets/script.js
+++ assets/script.js
@@ -45,3 +45,20 @@ function initMap(lat, lng) {
 window.initMap = initMap
 
 btnEl.addEventListener("click", formHandler)
+
+// // Update/add screens inside your module.exports:
+//     theme: {
+//       screens: {
+//         'sm': '640px',
+//         // => @media (min-width: 640px) { ... } 
+
+//         'md': '768px',
+//         // => @media (min-width: 768px) { ... }
+
+//         'lg': '1024px',
+//         // => @media (min-width: 1024px) { ... }
+
+//         'xl': '1280px',
+//         // => @media (min-width: 1280px) { ... }
+//       }
+//     }
